44a07ab7523ee424509e2b83b896f13b3ed547ea
Your Name <you@example.com>
2024-07-01 21:11:55 -0300

    entregaFinal

--- src/test/user.test.js
+++ src/test/user.test.js
@@ -1,6 +1,6 @@
 const UserApi = require('../api/user');
 const UserController = require('../controllers/user');
- 
+
 jest.mock('../controllers/user', () => ({
   createUser: jest.fn(),
   update: jest.fn(),
@@ -9,10 +9,10 @@ jest.mock('../controllers/user', () => ({
   login: jest.fn(),
   validateToken: jest.fn(),
 }));
- 
+
 describe('UserApi', () => {
   let req, res, next;
- 
+
   beforeEach(() => {
     req = {
       body: {},
@@ -26,7 +26,7 @@ describe('UserApi', () => {
     };
     next = jest.fn();
   });
- 
+
   describe('createUser', () => {
     it('Necessário criar um usuário válido', async () => {
       req.body = {
@@ -34,31 +34,31 @@ describe('UserApi', () => {
         email: 'luizvicente.parucker@outlook.com',
         senha: 'password',
       };
- 
+
       UserController.createUser.mockResolvedValue({ id: 1, nome: 'Luiz Vicente Parucker', email: 'luizvicente.parucker@outlook.com' });
- 
+
       await UserApi.createUser(req, res);
- 
+
       expect(res.status).toHaveBeenCalledWith(201);
       expect(res.send).toHaveBeenCalledWith({ id: 1, nome: 'Luiz Vicente Parucker', email: 'luizvicente.parucker@outlook.com' });
     });
- 
+
     it('Saber como contornar os erros de criação de usuário', async () => {
       req.body = {
         nome: 'Luiz',
         email: 'luizvicente.parucker@gmail.com',
         senha: 'password',
       };
- 
+
       UserController.createUser.mockRejectedValue(new Error('Database conexão erro'));
- 
+
       await UserApi.createUser(req, res);
- 
+
       expect(res.status).toHaveBeenCalledWith(400);
-      expect(res.send).toHaveBeenCalledWith({ error: 'Erro ao criar usuário Database erro de conexão' });
+      expect(res.send).toHaveBeenCalledWith({ error: 'Erro ao criar usuário Database conexão erro' });
     });
   });
- 
+
   describe('updateUser', () => {
     it('Necessário atualizar um usuário já criado', async () => {
       req.params.id = '1';
@@ -67,24 +67,20 @@ describe('UserApi', () => {
         email: 'novo.email@exemplo.com',
         senha: 'novasenha',
       };
- 
+
       UserController.update.mockResolvedValue({ id: 1, nome: 'Nome', email: 'novo.email@exemplo.com' });
- 
+
       await UserApi.updateUser(req, res);
- 
+
       expect(res.status).toHaveBeenCalledWith(200);
       expect(res.send).toHaveBeenCalledWith({ id: 1, nome: 'Nome', email: 'novo.email@exemplo.com' });
- 
+
       UserController.update.mockRejectedValue(new Error('Usuário não encontrado'));
- 
+
       await UserApi.updateUser(req, res);
- 
+
       expect(res.status).toHaveBeenCalledWith(400);
       expect(res.send).toHaveBeenCalledWith({ error: 'Erro ao alterar usuário Usuário não encontrado' });
     });
+  });
 });
-});
-  
- 
- 
- 
